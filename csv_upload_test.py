f4b4e347e6eda8edca01d14bca38e89c69048d8a
Tyler Martindale <68250419+martindalete@users.noreply.github.com>
2022-08-16 14:38:13 -0400

    Update csv_upload_test.py

--- csv_upload_test.py
+++ csv_upload_test.py
@@ -17,6 +17,7 @@ IFs['eISSN'] = IFs['eISSN'].str.replace('-', '')
 #create empty lists to which we will append API-gathered data
 ISSN_data = []
 eISSN_data = []
+identifiers = []
 csv = None
 
 @st.cache(suppress_st_warning=True)
@@ -55,38 +56,53 @@ def api_loop(dataframe):
             title = ''
         ISSN_data.append([DOI,ISSN,title])
         eISSN_data.append([DOI,eISSN,title])
+        identifiers.append([DOI,ISSN,title])
+        identifiers.append([DOI,eISSN,title])
         my_bar.progress(percent_complete)
     #st.write(ISSN_data)
     #st.write(eISSN_data)
     #assign ISSN/eISSN lists to dataframes
-    ISSN_df = pd.DataFrame(ISSN_data, columns = ['DOI','ISSN','Journal Title'])
-    eISSN_df = pd.DataFrame(eISSN_data, columns = ['DOI','eISSN','Journal Title'])
+    #ISSN_df = pd.DataFrame(ISSN_data, columns = ['DOI','ISSN','Journal Title'])
+    #eISSN_df = pd.DataFrame(eISSN_data, columns = ['DOI','eISSN','Journal Title'])
+    identifiers_df = pd.DataFrame(identifiers, columns = ['DOI','Identifier','Journal Title'])
     #st.dataframe(ISSN_df)
     #st.dataframe(eISSN_df)
+    #st.dataframe(identifiers_df)
     
     #merge (join) found data with JIF data
-    ISSN_left_merged = pd.merge(ISSN_df, IFs, how = "left", on=['ISSN', 'ISSN'])
-    eISSN_left_merged = pd.merge(eISSN_df, IFs, how = "left", on=['eISSN', 'eISSN'])
+    #ISSN_left_merged = pd.merge(ISSN_df, IFs, how = "left", on=['ISSN', 'ISSN'])
+    #eISSN_left_merged = pd.merge(eISSN_df, IFs, how = "left", on=['eISSN', 'eISSN'])
+    identifiers_merged_1 = pd.merge(identifiers_df, IFs, how = "left", left_on=['Identifier'], right_on=['eISSN'])
+    identifiers_merged_2 = pd.merge(identifiers_df, IFs, how = "left", left_on=['Identifier'], right_on=['ISSN'])
     
     #subset merged data to only show columns for DOI and JIF
-    ISSN_abbreviated = ISSN_left_merged[['DOI','Journal Impact Factor', 'Journal Title']]
-    eISSN_abbreviated = eISSN_left_merged[['DOI','Journal Impact Factor', 'Journal Title']]
+    #ISSN_abbreviated = ISSN_left_merged[['DOI','Journal Impact Factor', 'Journal Title']]
+    #eISSN_abbreviated = eISSN_left_merged[['DOI','Journal Impact Factor', 'Journal Title']]
+    identifiers_abbreviated_1 = identifiers_merged_1[['DOI','Journal Impact Factor', 'Journal Title']]
+    identifiers_abbreviated_2 = identifiers_merged_2[['DOI','Journal Impact Factor', 'Journal Title']]
     #st.dataframe(ISSN_abbreviated)
     #st.dataframe(eISSN_abbreviated)
-    
+    #st.dataframe(identifiers_abbreviated_2)
+    #st.dataframe(identifiers_abbreviated_1)
     #stack ISSN/eISSN dataframes on top of each other and then...
-    df_final = pd.concat([ISSN_abbreviated, eISSN_abbreviated])
-    df_final = df_final.reset_index(drop=True)
+    #df_final = pd.concat([ISSN_abbreviated, eISSN_abbreviated])
+    df_final_2 = pd.concat([identifiers_abbreviated_1, identifiers_abbreviated_2])
+    #df_final = df_final.reset_index(drop=True)
+    df_final_2 = df_final_2.reset_index(drop=True)
     
     #drop rows where JIF is empty
-    df_final = df_final[df_final['Journal Impact Factor'].notna()]
+    #df_final = df_final[df_final['Journal Impact Factor'].notna()]
+    df_final_2 = df_final_2[df_final_2['Journal Impact Factor'].notna()]
     #deduplicate rows 
-    df_final = df_final.drop_duplicates()
+    #df_final = df_final.drop_duplicates()
+    df_final_2 = df_final_2.drop_duplicates()
     
     #display final dataframe
-    st.dataframe(df_final)
+    #st.dataframe(df_final)
+    st.dataframe(df_final_2)
     
-    csv = convert_df(df_final)
+    #csv = convert_df(df_final)
+    csv = convert_df(df_final_2)
 
 @st.cache(suppress_st_warning=True)
 def show_download_button():
